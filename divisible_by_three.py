4f9eef7a3b664d57610e2f3d2c0e54945ab30605
Nixon Mangudya <iamnixon16@gmail.com>
2025-05-13 18:52:33 +0200

    did it

---
+++ divisible_by_three.py
@@ -0,0 +1,8 @@
+user_number = input('give a number')
+user_number = int(user_number)
+
+divisible_by_three = user_number / 5
+if user_number % 5 == 0:
+    print('yes')
+else:
+    print('no') 
---
+++ multiple_of_three.py
@@ -0,0 +1,8 @@
+user_number = input('give_number')
+user_number = float(user_number)
+
+multiple_of_three = user_number / 3
+if user_number % 3== 0:
+    print('yes')
+else:
+    print('no')
