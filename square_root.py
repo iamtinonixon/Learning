4b1d213bd53cf8474943d1ae967a2380ceb87f70
Nixon Mangudya <iamnixon16@gmail.com>
2025-07-08 21:42:31 +0200

    yes

--- square_root.py
+++
@@ -1,9 +0,0 @@
-def give_square_root(square):
-    square_root = user_number ** 2
-    return square_root
-
-user_number = input('give number')
-user_number = int(user_number)
-
-square_root = give_square_root(user_number)
-print(square_root)
